3122a33c082091fe504147869e0613ec57f3b5f2
Alexander (SASh) Alexiev <alexiev@gmail.com>
2011-03-14 11:48:49 +0200

    when livequery is present, we use an alternative triggering mechanism. Calling .uiinit will not do anything, because the components are authomatically initilized using livequery

--- jqueryui-init.js
+++ jqueryui-init.js
@@ -6,92 +6,117 @@
  * Dual licensed under the MIT or GPL Version 2 licenses.
  */
 (function($){
-	$.fn.attrs=function(prefix, list){
-		if (list){
-			var list = [];
-			$(this).each(function() {
-				var attributes = [];
-				for(var key in this.attributes) {
-					if(!isNaN(key)) {
-						if(!prefix || this.attributes[key].name.substr(0,prefix.length) == prefix) {
-							attributes.push(this.attributes[key].name);
+	$.extend($.fn, {
+		'attrs':function(prefix, list){
+			if (list){
+				var list = [];
+				$(this).each(function() {
+					var attributes = [];
+					for(var key in this.attributes) {
+						if(!isNaN(key)) {
+							if(!prefix || this.attributes[key].name.substr(0,prefix.length) == prefix) {
+								attributes.push(this.attributes[key].name);
+							}
 						}
 					}
-				}
-				list.push(attributes);
-			});
-			return (list.length > 1 ? list : list[0]);
-		}
-		else{
-			var res={};
-			var that = $(this)
-			$.each(that.attrs(prefix, true), function(i, v){
-				var k = v.substr(prefix.length).split('-')
-				var t = res
-				var l = false
-				for(var i in k){
-					if (k[i].length>0){
-						if (l) {if (typeof t[l] == 'undefined')t[l]={}; t = t[l]}
-						l = k[i]
+					list.push(attributes);
+				});
+				return (list.length > 1 ? list : list[0]);
+			}
+			else{
+				var res={};
+				var that = $(this)
+				$.each(that.attrs(prefix, true), function(i, v){
+					var k = v.substr(prefix.length).split('-')
+					var t = res
+					var l = false
+					for(var i in k){
+						if (k[i].length>0){
+							if (l) {if (typeof t[l] == 'undefined')t[l]={}; t = t[l]}
+							l = k[i]
+						}
 					}
+					var set = parseInt(that.attr(v))+'' == that.attr(v) ? parseInt(that.attr(v)) : ( parseFloat(that.attr(v))+'' == that.attr(v) ? parseFloat(that.attr(v)) : that.attr(v) )
+					t[l]=set;
+				})
+				return res
+			}
+		},
+		'_uiinit':function(element, inner, selfOnly){
+			var t;
+			if (selfOnly){
+				r = $();
+			}
+			else{
+				t = this.find('[data-ui-'+element+']')	
+			}
+			if ((!inner || selfOnly) && this.is('[data-ui-'+element+']')){ t = t.size() ? this : t.add(this); }
+			t.each(function(){
+				// Prevent dual init
+				if (!this.data('uiinited-'+element)){
+					this.data('uiinited-'+element, true)
+					// Initialize the widget
+					eval('$(this).'+element+'($(this).attrs("data-ui-'+element+'-"))')
 				}
-				var set = parseInt(that.attr(v))+'' == that.attr(v) ? parseInt(that.attr(v)) : ( parseFloat(that.attr(v))+'' == that.attr(v) ? parseFloat(that.attr(v)) : that.attr(v) )
-				t[l]=set;
 			})
-			return res
+			return this
+		},
+		'_uidestroy':function(element, inner){
+			var t;
+			if (selfOnly){
+				r = $();
+			}
+			else{
+				t = this.find('[data-ui-'+element+']')	
+			}
+			if ((!inner || selfOnly) && this.is('[data-ui-'+element+']')){ t = t.size() ? this : t.add(this); }
+			t.each(function(){
+				// Prevent dual destroy
+				if (this.data('uiinited-'+element)){
+					this.data('uiinited-'+element, false)
+					// Destroy the widget
+					eval('$(this).'+element+'("destroy")')
+				}
+			})
+			return this
+		},
+		'uiinit':function(inner, selfOnly){
+			var el = this
+			
+			$.each(uiinit, function(i, name){
+				el._uiinit(name, inner, selfOnly)
+			})
+			this.trigger('uiinit');
+			return this
+		},
+		'uidestroy':function(inner, selfOnly){
+			var el = this
+			$.each(uiinit, function(i, name){
+				el._uidestroy(name, inner, selfOnly)
+			})
+			this.trigger('uidestroy');
+			return this
 		}
-	}
-	$.fn._uiinit=function(element, inner){
-		var t = this.find('[data-ui-'+element+']')
-		if (!inner && this.is('[data-ui-'+element+']')){ t = t.size() ? this : t.add(this); }
-		t.each(function(){
-			// Initialize the widget
-			eval('$(this).'+element+'($(this).attrs("data-ui-'+element+'-"))')
-		})
-		return this
-	}
-	$.fn._uidestroy=function(element, inner){
-		var t = this.find('[data-ui-'+element+']')
-		if (!inner && this.is('[data-ui-'+element+']')){ t = t.size() ? this : t.add(this); }
-		t.each(function(){
-			// Destroy the widget
-			eval('$(this).'+element+'("destroy")')
-		})
-		return this
-	}
-	$.fn.uiinit=function(inner){
-		var el = this
-		this.data('uiinited-inner', true)
-		if (!inner) this.data('uiinited-self', true)
-		$.each(uiinit, function(i, name){
-			el._uiinit(name, inner)
-		})
-		this.trigger('uiinit');
-		return this
-	}
-	$.fn.uidestroy=function(inner){
-		var el = this
-		this.data('uiinited-inner', false)
-		if (!inner) this.data('uiinited-self', false)
-		$.each(uiinit, function(i, name){
-			el._uidestroy(name, inner)
-		})
-		this.trigger('uidestroy');
-		return this
-	}
-	$.fn.uiupdate=function(inner){
-		if ((!inner && this.data('uiinited-self')) || (inner && this.data('uiinited-inner'))){
-			this.uidestroy(inner)
-		}
-		return this.uiinit(inner)
-	}
+	})
+	
 	
 	var uiinit=[];
 	var domready = false;
 	$.uiinit=function(name){
-		uiinit.push(name);
-		// A fix that allows widgets added after the dom has loaded to be also initialized
-		if (domready) $('body')._uiinit(name)
+		if (typeof $.fn.livequery == 'function'){
+			// Register the initialization handler
+			$('*[data-ui-'+name+']').livequery(function(){
+				$(this)._uiinit(name, false, true)
+			}, function (){
+				$(this)._uidestroy(name, false, true)
+			})
+		}
+		else{
+			uiinit.push(name);
+			// A fix that allows widgets added after the dom has loaded to be also initialized
+			if (domready) $('body')._uiinit(name)
+		}
+		
 		return $
 	}
 	$.uiinit('button').uiinit('buttonset').uiinit('accordion').uiinit('datepicker').uiinit('dialog').uiinit('progressbar').uiinit('slider').uiinit('tabs')
